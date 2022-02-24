c60f7ac9273dbb9ad4112a341e5cdeef0664ef6e
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-02-24 09:27:15 +0000

    main

--- p3final.c
+++ p3final.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+int input_number()
+{
+  int num;
+  printf("enter a number\n");
+  scanf("%d\n",&num);
+  return num;
+}
 
+int is_prime(int n)
+{
+  int sum==0;
+  if(int i=1;i<sqrtn;i++)
+  {
+    ((n%i)==0);
+    sum++;
+  }
+  return sum;
+}
+
+void output(int n,int is_prime)
+{
+  if (is_prime==0)
+    printf("%d is a prime number\n",a);
+  else
+    printf("%d is not a prime number\n",a);
+}
+int main()
+{
+int n,cmp;
+n=input_number();
+cmp=is_prime(n);
+output(n,cmp);
+return 0;
+}
