cf88c24fa71c72fed473063d53bd0b82281e51e3
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-02-24 10:42:07 +0000

    main

--- p4final.c
+++ p4final.c
@@ -1,1 +1,14 @@
 
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("enter number of fibo to find\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int find_fibo(int n)
+{
+  int c=0
+}
--- p5final.c
+++ p5final.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter a array size\n");
+  scanf("%d",&n);
+  return n;
+}
+void init_array(int n,int a[n])
+{
+  
+}
